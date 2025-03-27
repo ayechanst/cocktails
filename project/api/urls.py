3cff6bee07c624ed44c9e4ee7adef7942b564d9b
ayechan <ayechan.sant@gmail.com>
2025-03-27 13:31:38 -0700

    html can browse alphabetically

--- project/api/urls.py
+++ project/api/urls.py
@@ -5,8 +5,8 @@ from . import views
 # Matches incoming URLs with view functions
 
 urlpatterns = [
-    # path('cocktails/', views.cocktail_view, name='single cocktail view'),
     path('cocktails/', views.cocktail_page, name='single cocktail view'),
+    # path('cocktails/', views.cocktail_view, name='single cocktail view'),
     path('cocktails/letter/', views.cocktail_letter_view, name='cocktail letter view'),
     # path('path that triggers the view', view function that is called if url matches)
 ]
--- project/api/utils.py
+++ project/api/utils.py
@@ -12,8 +12,23 @@ def get_cocktail_by_letter(letter):
     url = f"https://www.thecocktaildb.com/api/json/v1/1/search.php?f={letter}"
     response = requests.get(url)
     if response.status_code == 200:
-        data = response.json()
-        return data.get('drinks', [])
+        data = response.json().get('drinks', [])
+        trimmed_data = [
+            {
+                'id': drink.get('idDrink'),
+                'name': drink.get('strDrink'),
+                'instructions': drink.get('strInstructions'),
+                'glass': drink.get('strGlass'),
+                'ingredient1': drink.get('strIngredient1'),
+                'ingredient2': drink.get('strIngredient2'),
+                'ingredient3': drink.get('strIngredient3'),
+                'ingredient4': drink.get('strIngredient4'),
+                'ingredient5': drink.get('strIngredient5'),
+                'ingredient6': drink.get('strIngredient6'),
+                'ingredient7': drink.get('strIngredient7'),
+            }
+            for drink in data if drink
+        ]
+        return trimmed_data
     return []
-
 
